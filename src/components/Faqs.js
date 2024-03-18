42579bf871da75c2d05f7a8e60db2c4d6643066d
Puneet-Amrutkar <puneet.tech15@gmail.com>
2024-03-18 19:19:56 +0530

    vercel fixed

--- src/components/Faqs.js
+++ src/components/Faqs.js
@@ -33,23 +33,23 @@ const Faqs = () => {
             <p className='faqswiseheading'>How can Passion Labs benefit my school?</p>
             <p className='faqswisecontent'>Passion Labs provides schools with the necessary tools and support to enhance STEM, AI, and Robotics education, ultimately preparing students for future careers in these fields.</p>
         </div>
-        <div className='faqsbox'>
+        <div className='faqsbox-1'>
             <p className='faqswiseheading'>Are the kits provided by Passion Labs suitable for all grade levels?</p>
             <p className='faqswisecontent'>Yes, Passion Labs offers kits and curriculum that are tailored to different grade levels, ensuring that students of all ages can benefit from the program.</p>
         </div>
-        <div className='faqsbox'>
+        <div className='faqsbox-1'>
             <p className='faqswiseheading'>Is there a minimum requirement for schools to enroll in Passion Labs program?</p>
             <p className='faqswisecontent'>No, Passion Labs welcomes schools of all sizes and backgrounds to participate in the program, regardless of their current resources or infrastructure.</p>
         </div>
-        <div className='faqsbox'>
+        <div className='faqsbox-1'>
             <p className='faqswiseheading'>How can teachers access the in-person mentoring and virtual support?</p>
             <p className='faqswisecontent'>Teachers can easily reach out to Passion Labs team to schedule in-person mentoring sessions or to seek virtual support through the provided contact channels.</p>
         </div>
-        <div className='faqsbox'>
+        <div className='faqsbox-1'>
             <p className='faqswiseheading'>Can schools request customized curriculum based on their specific needs?</p>
             <p className='faqswisecontent'>Yes, Passion Labs offers the flexibility to customize curriculum based on school's specific needs and educational objectives, ensuring a tailored learning experience.</p>
         </div>
-        <div className='faqsbox'>
+        <div className='faqsbox-1'>
             <p className='faqswiseheading'>Are there any professional development opportunities for teachers?</p>
             <p className='faqswisecontent'>Yes, Passion Labs provides professional development opportunities for teachers to enhance their skills and knowledge in STEM, AI, and Robotics education.</p>
         </div>
